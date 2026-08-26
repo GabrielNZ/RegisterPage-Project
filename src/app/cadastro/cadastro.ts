156ee77b4e1884c779c74fef1af496cbacf9b458
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2026-08-26 12:54:19 -0300

    CREATED: Formulario in Cadastro Component

--- src/app/cadastro/cadastro.ts
+++ src/app/cadastro/cadastro.ts
@@ -1,7 +1,13 @@
 import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  imports: [],
+  imports: [MatCardModule, FormsModule, MatFormFieldModule, MatInputModule, MatIconModule, MatButtonModule],
   selector: 'app-cadastro',
   styleUrl: './cadastro.scss',
   templateUrl: './cadastro.html',
