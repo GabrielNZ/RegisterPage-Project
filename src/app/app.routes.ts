0105080eb8fcabce24b3d29063a6d60f08ea1d8c
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2026-08-26 11:28:09 -0300

    CREATED: Cadastro and Consulta Components with Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro';
+import { ConsultaComponent } from './consulta/consulta';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'cadastro', component:  CadastroComponent },
+    { path: 'consulta', component:  ConsultaComponent },
+];
