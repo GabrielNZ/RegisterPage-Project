6728e37f5ba575f974f1fdd998928227964c92f8
Gabriel Zimmermann <gabrielnzimmermann@gmail.com>
2026-08-26 11:59:53 -0300

    CREATED: toolbar + Angular Material Google implemented

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule} from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  imports: [RouterOutlet, MatToolbarModule],
+  imports: [RouterOutlet, RouterLink, MatToolbarModule, MatIconModule, MatButtonModule],
   selector: 'app-root',
   styleUrl: './app.scss',
   templateUrl: './app.html',
